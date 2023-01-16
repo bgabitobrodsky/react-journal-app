f7fba465eab99c4fb3dcf9e8ab758c81cebd691c
bgabitobrodsky <bernardogabito@gmail.com>
2023-01-16 11:57:09 -0300

    check authentication and custom hook

--- src/auth/layout/AuthLayout.js
+++ src/auth/layout/AuthLayout.js
@@ -12,14 +12,14 @@ export const AuthLayout = ({ children, title = '' }) => {
             sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
         >
             <Grid item
-				className="box-shadow"
-				xs={3}
-				sx={{
-					width: { md:450 },
-					backgroundColor: 'white', 
-					padding: 3, 
-					borderRadius: 2 
-				}}
+              className="box-shadow"
+              xs={3}
+              sx={{
+                width: { md:450 },
+                backgroundColor: 'white', 
+                padding: 3, 
+                borderRadius: 2 
+				      }}
             >
             	<Typography variant="h5" sx={{ mb: 1}}>{ title }</Typography>
             
