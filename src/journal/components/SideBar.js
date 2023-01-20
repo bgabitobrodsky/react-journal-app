a7b4cd57db01ff946ea94810d47d69d530e6ca21
bgabitobrodsky <bernardogabito@gmail.com>
2023-01-20 12:35:45 -0300

    showing notes in journal page

--- src/journal/components/SideBar.js
+++ src/journal/components/SideBar.js
@@ -1,46 +1,37 @@
-import { TurnedInNot } from "@mui/icons-material"
-import { Box, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
+import { Box, Drawer, List, Toolbar, Typography } from "@mui/material"
 import { useSelector } from "react-redux"
+import { SideBarItem } from "./SideBarItem";
 
 export const SideBar = ({drawerWidth = 240}) => {
 
-    const { displayName } = useSelector( state => state.auth );
+	const { displayName } = useSelector( state => state.auth );
+	const { notes } = useSelector( state => state.journal );
 
-  return (
-    <Box
-        component='nav'
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0} }}
-    >
-        <Drawer
-            variant='permanent'
-            open
-            sx={{ display: { xs: 'block' }, 
-                '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth}
-            }}
-        >
-            <Toolbar variant='h6' component='div'>
-                <Typography> { displayName }</Typography>
-            </Toolbar>
+	return (
+		<Box
+			component='nav'
+			sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0} }}
+		>
+			<Drawer
+				variant='permanent'
+				open
+				sx={{ display: { xs: 'block' }, 
+					'& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth}
+				}}
+			>
+				<Toolbar variant='h6' component='div'>
+					<Typography> { displayName }</Typography>
+				</Toolbar>
 
-            <List>
-                {
-                    ['Enero','Febrero','Marzo','Abril'].map( text => (
-                        <ListItem key={ text } disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <TurnedInNot/>
-                                </ListItemIcon>
-                                <Grid container>
-                                    <ListItemText primary={ text } />
-                                    <ListItemText secondary={ 'asdf adsf asdf asdf' }/>
-                                </Grid>
-                            </ListItemButton>
-                        </ListItem>
-                    ))
-                }
-            </List>
+				<List>
+					{
+						notes.map( note => (
+							<SideBarItem key={ note.id } {...note} />
+						))
+					}
+				</List>
 
-        </Drawer>
-    </Box>
-  )
+			</Drawer>
+		</Box>
+	)
 }
